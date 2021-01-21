bea4bab0a32d3915147b3c086809c94edb2244e4
BAibrahima <46569352+BAibrahima@users.noreply.github.com>
2021-01-21 21:45:36 +0100

    Update Dijkstra.R

--- R/Dijkstra.R
+++ R/Dijkstra.R
@@ -1,30 +1,4 @@
-
-
-
 library(igraph)
-
-
-g <- as.matrix(read.table(text=
-                            "C T1 V1 T2 V2 WS1 WS2 TC1 TC2 RRC
- C    0  8 10 12  0   0   0   0   0   0
- T1   8  0  2  0  0   0   0   0  12   0
- V1  10  2  0  0  2   0   0   0   0   0
- T2  12  0  0  0  0   5   0  10   0   0
- V2   0  0  2  0  0   1   7   0   0   0
- WS1  0  0  0  5  1   0   0   2   0   0
- WS2  0  0  0  0  7   0   0   2   2   0
- TC1  0  0  0 10  0   2   2   0   5   8
- TC2  0 12  0  0  0   0   2   5   0   3
- RRC  0  0  0  0  0   0   0   8   3   0", header=T))
-
-
-g <- graph.adjacency(g, mode="undirected", weighted=TRUE,diag = FALSE)
-str(g)
-#plot(g)
-
-plot(g,edge.label=E(g)$weight)
-
-
 
 #la fonction dijkstra donne la distance entre le noeud de d?part et de tous les autres noeuds.
 
